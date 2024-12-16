3d84e09579f3ec5f4a9e743af8a67d81483de8ba
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-12-16 11:55:02 +0600

    add mongodb and save users from clerk

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,5 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
+import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export async function POST(req) {
 	const SIGNING_SECRET = process.env.SIGNING_SECRET;
@@ -51,14 +53,37 @@ export async function POST(req) {
 	const { id } = evt.data;
 	const eventType = evt.type;
 
-	if (eventType === "user.created") {
-		console.log("user.created");
+	if (eventType === "user.created" || eventType === "user.updated") {
+		const {first_name, last_name, image_url, email_addresses} = evt?.data;
+		try {
+			const user = await createOrUpdateUser(id, first_name, last_name, image_url, email_addresses);
+
+			if (user && eventType === "user.created") {
+				try {
+					await clerkClient.user.updateUserMetadata (id, {
+						publicMetadata: {
+							userMongoId: user._id
+						}
+					})
+				} catch (error) {
+					console.log("Error: Could not update user metadata:", error);
+					return new Response("Error: Could not update user metadata", { status: 400 });
+				}
+			}
+		} catch (error) {
+			console.log("Error: Could not create or update user:", error);
+			return new Response("Error: Could not create or update user", { status: 400 });
+		}
 	}
-	if (eventType === "user.updated") {
-		console.log("user.updated");
-	}
+
 	if (eventType === "user.deleted") {
-		console.log("user.deleted");
+		try {
+			await deleteUser(id);
+		} catch (error) {
+			console.log("Error: Could not delete user:", error);
+			return new Response("Error: Could not delete user", { status: 400 });
+		}
+		
 	}
 
 	return new Response("Webhook received", { status: 200 });

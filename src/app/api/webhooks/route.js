88a47dc850376c70a62268e1fde283de48a375bd
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-12-16 12:07:30 +0600

    fix small typo in route js

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -54,25 +54,35 @@ export async function POST(req) {
 	const eventType = evt.type;
 
 	if (eventType === "user.created" || eventType === "user.updated") {
-		const {first_name, last_name, image_url, email_addresses} = evt?.data;
+		const { first_name, last_name, image_url, email_addresses } = evt?.data;
 		try {
-			const user = await createOrUpdateUser(id, first_name, last_name, image_url, email_addresses);
+			const user = await createOrUpdateUser(
+				id,
+				first_name,
+				last_name,
+				image_url,
+				email_addresses
+			);
 
 			if (user && eventType === "user.created") {
 				try {
-					await clerkClient.users.updateUserMetadata (id, {
+					await clerkClient.users.updateUserMetadata(id, {
 						publicMetadata: {
-							userMongoId: user._id
-						}
-					})
+							userMongoId: user._id,
+						},
+					});
 				} catch (error) {
 					console.log("Error: Could not update user metadata:", error);
-					return new Response("Error: Could not update user metadata", { status: 400 });
+					return new Response("Error: Could not update user metadata", {
+						status: 400,
+					});
 				}
 			}
 		} catch (error) {
 			console.log("Error: Could not create or update user:", error);
-			return new Response("Error: Could not create or update user", { status: 400 });
+			return new Response("Error: Could not create or update user", {
+				status: 400,
+			});
 		}
 	}
 
@@ -83,7 +93,6 @@ export async function POST(req) {
 			console.log("Error: Could not delete user:", error);
 			return new Response("Error: Could not delete user", { status: 400 });
 		}
-		
 	}
 
 	return new Response("Webhook received", { status: 200 });
